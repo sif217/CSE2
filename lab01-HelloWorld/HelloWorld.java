0dda601743ecdeecc5aec4bf5560420388fb1d92
sif217 <sif217@lehigh.edu>
2015-01-23 17:01:55 +0000

    updating HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,19 @@
+//Siyi Feng
+//Lab 1
+//Hello Word Java Program
+//
+//  define a class
+public class HelloWorld{
+//  add main method
+    public static void main(String[] args) {
+//      print the statement with box around it
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("#  Hello World  2015!  #");
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("test");
+        
+        //test/testtest
+    }
+}
