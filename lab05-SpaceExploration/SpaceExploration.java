3227814ac2f775694353f5fc0a3c4e91b912d4a6
sif217 <sif217@lehigh.edu>
2015-03-20 15:20:44 +0000

    updated lab05

--- lab05-SpaceExploration/SpaceExploration.java
+++ lab05-SpaceExploration/SpaceExploration.java
@@ -8,43 +8,42 @@ of 2000 to 2010, and list all the space exploration advances
 //define a class
 public class SpaceExploration{
     // define a method
-    public static void mian(String[] args){
-        int year1=(int) (Math.random()*11)+2000;
-        int year2=(int) (Math.random()*11)+2000;
-        System.out.print("Here is a timeline of space exploration events from "+year1+" to "+year2);
+    public static void main(String[] args){
+        int year=(int) (Math.random()*11)+2000;
+        
+        System.out.println("Here is a timeline of space exploration events from "+year+" to " +2000);
         switch (year){
-            case 2000:
-                System.out.print("2000: First spacecraft orbits an asteroid");
-                break;
-            case 2001:
-                System.out.print("2001: First spacecraft lands on asteroid");
-                break;
-            case 2002:
-                System.out.print("2002: N/A");
-                break;
-            case 2003:
-                System.out.print("2003: Largest infrared telescope released");
-                break;
+           
+            case 2010:
+                System.out.println("2010: SpaceX sucessfully sends spacecraft to orbit and back");
+               
+            case 2009:
+                System.out.println("2009: Spacecraft collies with comet");
+                
+            case 2008:
+                System.out.println("2008: Kepler launched to study deep space");
+                
+            case 2006:
+                System.out.println("2005: Spacecraft returns with collections from a comet");
+                
+            case 2005:
+                System.out.println("2005: Spacecraft collies with comet");
+              
             case 2004:
                 System.out.print("2004: First spacecraft orbits an asteroid");
+               
+            case 2003:
+                System.out.println("2003: Largest infrared telescope released");
+                
+            case 2002:
+                System.out.println("2002: N/A");
+               
+            case 2001:
+                System.out.println("2001: First spacecraft lands on asteroid");
+        
+            case 2000:
+                System.out.println("2000: First spacecraft orbits an asteroid");
                 break;
-            case 2005:
-                System.out.print("2005: Spacecraft collies with comet");
-                break;
-            case 2006:
-                System.out.print("2005: Spacecraft returns with collections from a comet");
-                break;
-            
-            case 2008:
-                System.out.print("2008: Kepler launched to study deep space");
-                break;
-            case 2009:
-                System.out.print("2009: Spacecraft collies with comet");
-                break;
-            case 20010:
-                System.out.print("2010: SpaceX sucessfully sends spacecraft to orbit and back");
-                break;
-            
         }
     }
 }
